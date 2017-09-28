ba0e72f5c3bb97ccf836ad5d67638a6df38999b5
Christof Kälin <christof.kaelin@gmail.com>
2017-09-28 17:15:47 +0200

    comments and exec files

--- 004_parse_execute/03_ParseFiles/main.go
+++ 004_parse_execute/03_ParseFiles/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	tpl, err := template.ParseFiles("one.gmao")
+	tpl, err := template.ParseFiles("one.gmao") // first call on library object
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -17,12 +17,12 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	tpl, err = tpl.ParseFiles("two.gmao", "vespa.gmao")
+	tpl, err = tpl.ParseFiles("two.gmao", "vespa.gmao") // subsequent calls on own tpl object
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gmao", nil)
+	err = tpl.ExecuteTemplate(os.Stdout, "vespa.gmao", nil) // execute specific template (loaded before)
 	if err != nil {
 		log.Fatalln(err)
 	}
