6c33d9a2fe535f9554efae905ec037375b4e7dd5
Christof Kälin <christof.kaelin@gmail.com>
2017-11-09 18:53:01 +0100

    trying bson id

--- 046_mongodb/16_go-mongo/books/models.go
+++ 046_mongodb/16_go-mongo/books/models.go
@@ -12,10 +12,10 @@ import (
 type Book struct {
 	// add ID and tags if you need them
 	// ID     bson.ObjectId // `json:"id" bson:"_id"`
-	Isbn   string  // `json:"isbn" bson:"isbn"`
-	Title  string  // `json:"title" bson:"title"`
-	Author string  // `json:"author" bson:"author"`
-	Price  float32 // `json:"price" bson:"price"`
+	Isbn   string  `json:"isbn" bson:"isbn"`
+	Title  string  `json:"title" bson:"title"`
+	Author string  `json:"author" bson:"author"`
+	Price  float32 `json:"price" bson:"price"`
 }
 
 func AllBooks() ([]Book, error) {
